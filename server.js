94bd26cc37147a1ffb90d70794cfb2588f25ec24
paszo <paszo2008@gmail.com>
2020-06-30 17:28:09 +0200

    Changed response for main route

--- server.js
+++ server.js
@@ -28,9 +28,10 @@ app.use(express.json());
 
 app.use(cors());
 
-app.get('/', (req, res) => {getallusers.handleGetAll(req, res, db)})
+// app.get('/', (req, res) => {getallusers.handleGetAll(req, res, db)});
+app.get('/', (req, res) => {res.send('it is working')});
 
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)});
 
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
 
